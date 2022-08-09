7fe0a6fb1e3b64fc620b272c2d537f38707fc08a
harsh1532 <patelharsh1532@gmail.com>
2022-08-09 12:47:22 -0400

    main

--- flask_server/python files/server.py
+++ flask_server/python files/server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from get_arb_opp import get_arbitrage_opps
+from flask_cors import CORS, cross_origin
 
 
-app = Flask(__name__)
-
+app = Flask(__name__,static_folder='my-app/bulild',static_url_path='')
+CORS(app)
 
 @app.route('/members')
 def members():
@@ -13,12 +14,16 @@ def members():
 def fun():
     return "Don't worry ...your server is live"
 @app.route('/arbitrage')
+@cross_origin()
 def arbitrage():
     print('here')
     print(get_arbitrage_opps())
     print('ieweo')
     return get_arbitrage_opps()
-    
+@app.route('/')
+@cross_origin() 
+def server():
+    return send_from_directory(app.static_folder,'index.html')
     
 
 if __name__ == '__main__':
